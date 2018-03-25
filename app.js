6eadd8fad4d22bebc0869e97236a57cfae9d5c5d
Satinder Singh <agnosticode@gmail.com>
2018-03-25 14:18:05 +0530

    Sub classes- inheritance in ES6

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 class Person{
-    constructor(firstName,lastName,dob){
+    constructor(firstName,lastName){
         this.firstName=firstName;
         this.lastName=lastName;
-        this.birthday=new Date(dob);
     };
 
     greeting(){
         return `Hello there ${this.firstName} ${this.lastName}`;
     };
-
-    calculateAge(){
-        const diff=Date.now()-this.birthday.getTime();
-        const ageDate=new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear()-1970);
-    };
-
-    getsMarried(newLastName){
-        this.lastName=newLastName;
-    };
-
-    static addNumbers(x,y){
-        return x+y;
-    };
 };
 
-const mary= new Person('Mary','Williams','25 july 1994');
+class Customer extends Person {
+    constructor(firstName,lastName,phone,membership){
+        super(firstName,lastName);
 
-mary.getsMarried('Smith');
-console.log(mary);
+        this.phone=phone;
+        this.membership=membership;
+    };
+    static getMembershipCost(){
+        return 500;
+    }
 
-//Calling Static Method
-console.log(Person.addNumbers(2,4));
+};
+
+const john=new Customer('John','Doe','555-55-555','Standard');
+
+console.log(john);
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
